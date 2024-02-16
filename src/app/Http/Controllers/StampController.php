cd76cfb94e0072e130f791fb7c480c07d7bd0c79
aruma-joishi <aru-jo@docmo.ne.jp>
2024-02-16 21:23:25 +0900

    打刻機能

--- src/app/Http/Controllers/StampController.php
+++ src/app/Http/Controllers/StampController.php
@@ -20,33 +20,42 @@ class StampController extends Controller
 
     public function attend(Request $request)
     {
-        $attend = Carbon::now();
-        Attend::upadate($attend);
-        $users = User::with('name');
-        return view('index', compact('users'));
+        $users = Attend::get('user_id');
+        foreach ($users as $user) {
+            if ($user['user_id'] == $request->get('user_id')){
+                return redirect('/')->with('message', '$attend');
+            }
+        }
+
+        $attend = $request->all();
+        $attend['attend'] = Carbon::now();
+        Attend::create($attend);
+        return redirect('/')->with('message', '$attend');
     }
+
 
     public function leave(Request $request)
     {
-        $attend = $request->only(['leave']);
-        $users = User::with('name');
-        $leave = Carbon::now();
-        return view('index', compact('users'));
-    }
+        $attend = $request->all();
+        $attend['leave'] = Carbon::now();
+        
+        Attend::find($request->user_id)->update($attend);
+        return redirect('/')->with('message', '$attend');
+        }
 
     public function breakbegin(Request $request)
     {
-        $attend = $request->only(['breakbegin']);
-        $users = User::with('name');
-        $breakbegin = Carbon::now();
-        return view('index', compact('users'));
+        $attend = $request->all();
+        $attend['breakbegin'] = Carbon::now();
+        Attend::find($request->user_id)->update($attend);
+        return redirect('/')->with('message', '$attend');
     }
 
     public function breakend(Request $request)
     {
-        $attend = $request->only(['breakend']);
-        $users = User::with('name');
-        $breakend = Carbon::now();
-        return view('index', compact('users'));
+        $attend = $request->all();
+        $attend['breakend'] = Carbon::now();
+        Attend::update($attend);
+        return redirect('/')->with('message', '$attend');;
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -13,27 +13,28 @@
   </div>
 
   <div class="stamp">
-    <form class="stamp__btn" action="/attend" method="post">
-      @method('PATCH')
+    <form class="stamp__button" action="/attend" method="post">
       @csrf
       <button class="stamp__button__submit" type="submit">勤務開始</button>
-      <input type="hidden" name="id" value="{{ Auth::user()->id }}">
+      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
     </form>
     <form class="stamp__button" action="/leave" method="post">
       @method('PATCH')
       @csrf
       <button class="stamp__button-submit" type="submit">勤務終了</button>
-      <input type="hidden" name="id" value="{{ Auth::user()->id }}">
+      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
     </form>
     <form class="stamp__button" action="/breakbegin" method="post">
       @method('PATCH')
       @csrf
       <button class="stamp__button-submit" type="submit">休憩開始</button>
+      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
     </form>
     <form class="stamp__button" action="/breakend" method="post">
       @method('PATCH')
       @csrf
       <button class="stamp__button-submit" type="submit">休憩終了</button>
+      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
     </form>
   </div>
 </div>
